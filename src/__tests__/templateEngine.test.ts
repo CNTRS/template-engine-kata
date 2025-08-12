ba6dd26c266d64925b71800db823a36386a3d5b4
CNTRS <m.contreras.calzon@gmail.com>
2025-08-13 00:07:38 +0200

    Add warnings logic for edge cases

--- src/__tests__/templateEngine.test.ts
+++ src/__tests__/templateEngine.test.ts
@@ -38,7 +38,7 @@ describe("template engine", () => {
         const result = parse(template, variables)
         expect(result.text).toBe("This is a template with a variable: ")
     })
-    it.skip("parses template with variable as null", () => {
+    it("parses template with variable as null", () => {
         const template = "This is a template with a variable: ${variable}"
         const variables = { variable: null }
         const result = parse(template, variables)
@@ -52,14 +52,14 @@ describe("template engine", () => {
         expect(result.text).toBe(template)
         expect(result.warnings[0]).toBe("Variable user not found")
     })
-    it.skip("warns about variable not found in template", () => {
+    it("warns about variable not found in template", () => {
         const template = "This is a template with a variable: ${variable}"
         const variables = { user: "value" }
         const result = parse(template, variables)
         expect(result.text).toBe(template)
         expect(result.warnings[0]).toBe("Variable user not found in template")
     })
-    it.skip("warns about variable null or empty arguments", () => {
+    it("warns about variable null or empty arguments", () => {
         const template = null
         const variables = null //@ts-ignore
         const result = parse(template, variables)
--- src/core/templateEngine.ts
+++ src/core/templateEngine.ts
@@ -3,16 +3,37 @@ type ParseResponse = {
     warnings: string[]
 }
 
+function getVariableValue(variables: Record<string, any>, variableKey: string): string {
+    if (variables[variableKey] === null) return "null"
+    return variables[variableKey] ?? ""
+}
+
 export function parse(template: string, variables: Record<string, any>): ParseResponse {
 
     const warnings = []
     let parsedText = template ?? ""
 
+    if (template == null || template === "") {
+        parsedText = ""
+        warnings.push("Template is null or empty")
+    }
+
+    if (variables == null) {
+        warnings.push("Variables are null or empty")
+    }
+
     if (variables && Object.keys(variables).length > 0) {
         Object.keys(variables).forEach(variableKey => {
             const templateVariable = `\$\{${variableKey}\}`
-            const variableValue = variables[variableKey] ?? ""
+            const variableValue = getVariableValue(variables, variableKey)
 
+            if (variables[variableKey] === null) {
+                warnings.push(`Variable ${variableKey} is null`)
+            }
+
+            if (!parsedText.includes(templateVariable)) {
+                warnings.push(`Variable ${variableKey} not found in template`)
+            }
             parsedText = parsedText.replace(templateVariable, variableValue)
         })
     }
